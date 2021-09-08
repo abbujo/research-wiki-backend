61e4b1d7f87575b3c119ca83bfd41fb5d47dfa97
abhishek joshi <joshi.abj13@.gmail.com>
2021-09-08 18:09:40 +1000

    Added all changes 3

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
-import flask
 from flask import request, jsonify, Flask
 from pymongo import MongoClient
 from flask_cors import CORS, cross_origin
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -46,4 +46,5 @@ def api_label():
     return jsonify(results)
 
 if __name__=="__main__":
-  app.run()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
